e49419c50914bc2b0b9596376b3787d021b2768f
Kevin Litwack <kevin.litwack@gmail.com>
2019-12-04 19:42:20 -0800

    Day 4 solutions

--- src/advent_of_code_2019/day04.clj
+++ src/advent_of_code_2019/day04.clj
@@ -1,1 +1,27 @@
 (ns advent-of-code-2019.day04)
+
+
+(defn check-password
+  [min max n]
+  (let [digits (map #(Integer/parseInt (str %)) (str n))
+        digit-pairs (partition 2 1 digits)]
+    (and
+      (<= min n max)
+      (some (partial apply =) digit-pairs)
+      (every? (partial apply <=) digit-pairs))))
+
+(defn count-passwords
+  [min max check-fn]
+  (->> (range min (inc max))
+       (filter (partial check-fn min max))
+       (count)))
+
+(defn check-password-2
+  [min max n]
+  (let [digits (map #(Integer/parseInt (str %)) (str n))]
+    (and
+      (<= min n max)
+      (->> (partition-by identity digits)
+           (map count)
+           (some (partial = 2)))
+      (every? (partial apply <=) (partition 2 1 digits)))))
